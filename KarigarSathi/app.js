7015189d23cccb7403b19adfb507cd26c28a2587
Utkarsh Dixit <utkarshdixit.2k21@gmail.com>
2024-02-10 04:40:42 +0530

    login done

--- KarigarSathi/app.js
+++ KarigarSathi/app.js
@@ -3,28 +3,23 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const passport = require('passport');
 var Session = require('express-session');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-const passport = require('passport');
 
 var app = express();
 
+
+const users = require('./routes/users');
 const localStrategy = require('passport-local');
 passport.use(new localStrategy(usersRouter.authenticate()));
+
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 app.use(Session({
   resave: false,
@@ -38,6 +33,16 @@ app.use(passport.session());
 passport.serializeUser(usersRouter.serializeUser());
 passport.deserializeUser(usersRouter.deserializeUser());
 
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+passport.use(new localStrategy(usersRouter.authenticate()));
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
