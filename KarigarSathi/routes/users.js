01dde8b6ab5bd61be1a708dfcbf13a3c9bbe0b95
Utkarsh Dixit <utkarshdixit.2k21@gmail.com>
2024-02-09 23:01:25 +0530

    mongodb setup

--- KarigarSathi/routes/users.js
+++ KarigarSathi/routes/users.js
@@ -1,9 +1,38 @@
-var express = require('express');
-var router = express.Router();
+const mongoose = require('mongoose');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+mongoose.connect("mongodb+srv://utkarshhdixit:nXPmhBvjeo60ieQ9@cluster0.plsuptg.mongodb.net/?retryWrites=true&w=majority");
+
+const userSchema = new mongoose.Schema({
+  profession: String,
+  selectedOption: String,
+  mobile: {
+    type: Number,
+    required: true
+  },
+  name: {
+    type: String
+  }, 
+  username: {
+    type: String,
+    unique: true,
+  },
+  locationName: {
+    type: String
+  },
+  lat: Number,
+  long: Number,
+  email: {
+    type: String,
+    unique: true,
+  },
+  password: {
+    type: String,
+  },
+  registrationDate: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
-module.exports = router;
+module.exports = mongoose.model("User", userSchema);
+
