4ffe95abf33ad78f953920d90405c7a9ebb2cada
shashank-mishra14 <shashank.mishra.dev14@gmail.com>
2024-02-10 19:15:48 +0530

    changes in ui

--- KarigarSathi/routes/users.js
+++ KarigarSathi/routes/users.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const plm = require('passport-local-mongoose');
 
-mongoose.connect("mongodb://localhost:27017/karigarsathi");
+mongoose.connect("mongodb+srv://karigarhtf:Fl96fWL659Jobb43@cluster0.i7zhlid.mongodb.net/");
 
 const userSchema = new mongoose.Schema({
   profession: String,
